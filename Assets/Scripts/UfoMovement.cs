86792c90cebc7c021cd88aafe841d4e7239c8e7c
Celfi <Celfi>
2016-08-06 22:43:32 -0400

    Title:  - Added walls  - Added Ufo  - Added movement to Ufo  - Added rigidBody2D to Ufo and walls Developer: Celso

---
+++ Assets/Scripts/UfoMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UfoMovement : MonoBehaviour {
+
+	public float forceMultiplier;
+	private Rigidbody2D ufoRigidBody;
+
+	// Use this for initialization
+	void Start () {
+	
+		ufoRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Called before to apply physics
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 forceVector = new Vector2 (moveHorizontal, moveVertical);
+
+		ufoRigidBody.AddForce (forceVector * forceMultiplier);
+	}
+}
